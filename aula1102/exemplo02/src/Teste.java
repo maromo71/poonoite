db532b7c58c55e2ee1e3969734c3411d15d3c59a
maromo71 <professormoraes@gmail.com>
2025-02-11 22:07:52 -0300

    Aula do dia 11 de fevereiro de 2025

---
+++ aula1102/exemplo02/src/Teste.java
@@ -0,0 +1,9 @@
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("Segundo programa");
+        int a = 10;
+        int b = 30;
+        int soma = a + b;
+        System.out.printf("Soma de %d + %d = %d\n", a, b , soma);
+    }
+}
