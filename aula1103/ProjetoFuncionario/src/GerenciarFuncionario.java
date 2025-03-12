e402582332e13efb64819c2c0dabefe5d292a5e2
maromo71 <professormoraes@gmail.com>
2025-03-11 21:53:24 -0300

    Aula do dia 11 de marco

---
+++ aula1103/ProjetoFuncionario/src/GerenciarFuncionario.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GerenciarFuncionario {
+    private List<Funcionario> funcionarios = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+    }
+
+    public void execConsultar(){
+
+    }
+    public void execBonificar(){
+
+    }
+    public void execCadastrar(){
+
+    }
+}
