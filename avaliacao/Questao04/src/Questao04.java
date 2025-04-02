ba9ac8c7dfabfe7ec5c49c71c276cd81765c2da2
maromo71 <professormoraes@gmail.com>
2025-04-02 15:23:18 -0300

    Acerto do gabarito

--- avaliacao/Questao04/src/Questao04.java
+++ avaliacao/Questao04/src/Questao04.java
@@ -4,7 +4,7 @@ public class Questao04 {
     //II. O comando import é utilizado para importar classes de outros pacotes para o codigo-fonte atual. [V]
     //III. O pacote padrao de Java é o java.lang, e suas classes podem ser usadas sem a necessidade de importacao. [V]
     //IV. Classes de um pacote packageA só podem ser acessadas por classes de outro pacote packageB se forem publicas e importadas corretamente. [V]
-    //V. Em Java, todos os arquivos .java devem obrigatoriamente pertencer a um pacote. [V]
+    //V. Em Java, todos os arquivos .java devem obrigatoriamente pertencer a um pacote. [F]
 
-    //Entao a E) todas as alternativas estão corretas.
+    //Entao é a a) I, II, III e IV estão corretas..
 }
