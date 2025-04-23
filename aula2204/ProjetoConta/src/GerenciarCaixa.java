997150bfcb6a92b49063448e9d41cc49071b7d22
maromo71 <professormoraes@gmail.com>
2025-04-22 22:04:51 -0300

    Aula do dia 22 de abril

--- aula2204/ProjetoConta/src/GerenciarCaixa.java
+++ aula2204/ProjetoConta/src/GerenciarCaixa.java
@@ -5,14 +5,14 @@ import javax.swing.*;
 
 public class GerenciarCaixa {
     public static void main(String[] args) {
-        //TelaMovimento tela = new TelaMovimento();
-        //tela.setVisible(true);
-        JFrame minhaTela = new JFrame();
-        minhaTela.setContentPane(new Tela().getFormCaixa());
-        minhaTela.setSize(350,400);
-        minhaTela.setTitle("Controle de Caixa");
-        minhaTela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        minhaTela.setLocationRelativeTo(null);
-        minhaTela.setVisible(true);
+        TelaMovimento tela = new TelaMovimento();
+        tela.setVisible(true);
+       // JFrame minhaTela = new JFrame();
+      //  minhaTela.setContentPane(new Tela().getFormCaixa());
+      //  minhaTela.setSize(350,400);
+     //   minhaTela.setTitle("Controle de Caixa");
+      //  minhaTela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     //   minhaTela.setLocationRelativeTo(null);
+      //  minhaTela.setVisible(true);
     }
 }
