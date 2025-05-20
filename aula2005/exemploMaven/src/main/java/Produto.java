fcba0514d91525c71dc36b209d1a9335f2857dea
maromo71 <professormoraes@gmail.com>
2025-05-20 19:57:32 -0300

    Aula do dia 20 de maio

---
+++ aula2005/exemploMaven/src/main/java/Produto.java
@@ -0,0 +1,14 @@
+import lombok.*;
+
+
+@Data
+public class Produto {
+    private int idProduto;
+    private String nomeProduto;
+    private String descricaoProduto;
+    private double precoProduto;
+    private boolean ativo;
+
+
+
+}
